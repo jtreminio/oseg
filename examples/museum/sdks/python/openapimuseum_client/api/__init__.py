ac1d294202eba28ff78695065383a24983613ad1
Juan Treminio <jtreminio@gmail.com>
2025-02-05 11:41:31 -0600

    Make bin build tools useful for more projects; remove museum SDK files

--- examples/museum/sdks/python/openapimuseum_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapimuseum_client.api.events_api import EventsApi
-from openapimuseum_client.api.operations_api import OperationsApi
-from openapimuseum_client.api.tickets_api import TicketsApi
-
